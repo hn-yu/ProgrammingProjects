15d81333e872a77dc3ad893d1bccba64a7eaa819
Haonan Yu <yhn90116@gmail.com>
2026-09-21 18:18:17 +0800

    04 start

---
+++ Project#04/sol.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+namespace fs = std::filesystem;
+fs::path root = "./input/h2o/STO-3G";
+fs::path t_file   = root / "t.dat";
+fs::path v_file   = root / "v.dat";
+fs::path eri_file = root / "eri.dat";
+fs::path geom_file = root / "geom.dat";
